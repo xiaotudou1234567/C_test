b325b0e8f8abe92ef814b8f3a67a56cf48c36972
gaoyuchao <1207058426@qq.com>
2024-04-16 20:08:08 +0800

    更新的测试

--- C_test_20240416/C_test_20240416/hello.c
+++ C_test_20240416/C_test_20240416/hello.c
@@ -1,7 +1,32 @@
+#define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
+
+int campare_max(int x, int y)
+{
+	if (x >= y)
+	{
+		return x;
+	}
+	else
+	{
+		return y;
+	}
+}
 
 int main()
 {
-	printf("hello\n");
+	int num1 = 0, num2 = 0;
+
+	while (scanf("%d %d", &num1,&num2) != EOF)
+	{
+		printf("%d\n", campare_max(num1, num2));
+	}
 	return 0;
 }
+
+//
+//int main()
+//{
+//	printf("hello\n");
+//	return 0;
+//}
