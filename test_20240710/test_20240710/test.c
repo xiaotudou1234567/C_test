18b2c7b8e2b637cba6a3e95449e34849af1b3d36
gaoyuchao <1207058426@qq.com>
2024-07-16 21:28:37 +0800

    静态库

--- test_20240710/test_20240710/test.c
+++ test_20240710/test_20240710/test.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <math.h>
 
 void printtable(int n)
 {
@@ -28,13 +29,31 @@ int is_year(int n)
 	return ((n % 400 == 0) || (n % 4 == 0 && n % 100 != 0)) ? 1 : 0;
 }
 
+void is_prime(int n)
+{
+	int i,flag = 1;
+	for (i = 2; i <= sqrt(n); i++)
+	{
+		if (n % i == 0)
+		{
+			flag = 0;
+			break;
+		}
+	}
+	if (flag == 1)
+	{
+		printf("%d ", n);
+	}
+}
+
+void conpare(int a, int b)
+{
+	int n = (a > b) ? a : b;
+	printf("%d", n);
+}
+
 int main()
 {
-	int a,b = 0;
-	while (scanf("%d", &a) != EOF)
-	{
-		is_year(a)? printf("ÊÇ\n"): printf("·ñ\n");
-	}
-
+	printf("%d", printf("%d", printf("%d", 43)));
 	return 0;
 }
---
+++ test_20240716/test_20240716/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#pragma comment(lib,"test_20210715.lib") 
+
+int main()
+{
+	print_int(234);
+	return 0;
+}
